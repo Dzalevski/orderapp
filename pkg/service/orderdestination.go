eaa6cfc4e20012088d9ee05d1225de565ddb50fe
Darko Djalevski <darko.djalevski@inplayer.com>
2020-06-20 14:56:24 +0200

    todo

---
+++ pkg/service/orderdestination.go
@@ -0,0 +1,3 @@
+package service
+
+//todo func which will take order(consID and customerID) will take via google api customer longitude and latitude(by geo_location City) and calculate which van should take cons to customer
